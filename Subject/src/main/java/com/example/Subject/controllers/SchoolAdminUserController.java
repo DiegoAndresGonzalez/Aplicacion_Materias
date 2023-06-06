1dc1d5bd65e0d86d7a8c2ee3a20346f36c858aef
Diego <deworky@gmail.com>
2023-06-06 16:44:35 -0500

    Añadiendo la clase SchoolSubject referenciando a las Materias

---
+++ Subject/src/main/java/com/example/Subject/controllers/SchoolAdminUserController.java
@@ -0,0 +1,10 @@
+package com.example.Subject.controllers;
+
+import com.example.Subject.models.SchoolAdminUser;
+import com.example.Subject.repositories.SchoolAdminUserRepository;
+
+public class SchoolAdminUserController {
+    private SchoolAdminUserRepository schoolAdminUserRepository;
+
+    }
+
---
+++ Subject/src/main/java/com/example/Subject/models/SchoolSubject.java
@@ -0,0 +1,4 @@
+package com.example.Subject.models;
+
+public class SchoolSubject {
+}
