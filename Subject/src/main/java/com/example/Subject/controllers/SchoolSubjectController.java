e1c47396eb0a416a119d212178fd4bee57d7c0ce
Diego <deworky@gmail.com>
2023-06-06 17:33:54 -0500

    Finalizada la creación del método crear materia

--- Subject/src/main/java/com/example/Subject/controllers/SchoolSubjectController.java
+++ Subject/src/main/java/com/example/Subject/controllers/SchoolSubjectController.java
@@ -3,19 +3,26 @@ package com.example.Subject.controllers;
 import com.example.Subject.models.SchoolSubject;
 import com.example.Subject.repositories.SchoolAdminUserRepository;
 import com.example.Subject.repositories.SchoolSubjectRepository;
+import com.example.Subject.services.SchoolSubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/subject")
 public class SchoolSubjectController {
     @Autowired
-    private SchoolSubjectRepository schoolSubjectRepository;
+    private SchoolSubjectService schoolSubjectService;
 
     @GetMapping()
     public ArrayList<SchoolSubject> getSchoolSubject(){
-        return
+        return schoolSubjectService.getSchoolSubject();
+    }
+
+    @PostMapping()
+    public SchoolSubject createSubject(@RequestBody SchoolSubject subject){
+        return this.schoolSubjectService.createSubject(subject);
     }
 }
--- Subject/src/main/java/com/example/Subject/services/SchoolSubjectService.java
+++ Subject/src/main/java/com/example/Subject/services/SchoolSubjectService.java
@@ -1,4 +1,25 @@
 package com.example.Subject.services;
 
-public class SchoolSubjectService {
+import com.example.Subject.models.SchoolSubject;
+import com.example.Subject.repositories.SchoolSubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@Service
+public
+class SchoolSubjectService {
+    @Autowired
+    SchoolSubjectRepository schoolSubjectRepository;
+
+    public ArrayList<SchoolSubject> getSchoolSubject(){
+        return(ArrayList<SchoolSubject>) schoolSubjectRepository.findAll();
+    }
+    public SchoolSubject createSubject(SchoolSubject subject){
+        return schoolSubjectRepository.save(subject);
+    }
 }
