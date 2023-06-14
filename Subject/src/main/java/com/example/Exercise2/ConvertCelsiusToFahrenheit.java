9226a071fc40c543ceb9fb058f97d7a6a03986b1
Diego <deworky@gmail.com>
2023-06-14 13:36:46 -0500

    Creando el paquete Exercise2 y la clase ConvertCelsiusToFahrenheit, creando la función CelsiusToFahrenheit, instanciado la clase ConvertCelsiusToFahrenheit e inicializando el scanner para devolver un resultado

---
+++ Subject/src/main/java/com/example/Exercise2/ConvertCelsiusToFahrenheit.java
@@ -0,0 +1,10 @@
+package com.example.Exercise2;
+
+public class ConvertCelsiusToFahrenheit {
+
+    public double convertCelsiusToFahrenheit(double celsius){
+        double fahrenheit = (celsius * 1.8) + 32;
+        return fahrenheit;
+    }
+
+}
--- Subject/src/main/java/com/example/Subject/SubjectApplication.java
+++ Subject/src/main/java/com/example/Subject/SubjectApplication.java
@@ -1,13 +1,26 @@
 package com.example.Subject;
 
+import com.example.Exercise2.ConvertCelsiusToFahrenheit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SubjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubjectApplication.class, args);
+
+		System.out.println("Exercise 2: Convertir grados Centigrados a Farenheit.");
+		Scanner scanCelsius = new Scanner(System.in);
+		System.out.println("Ingrese a continuación los grados Centigrados que desea convertir a Farenheit: ");
+		double celsius = scanCelsius.nextDouble();
+		ConvertCelsiusToFahrenheit exercise2 = new ConvertCelsiusToFahrenheit();
+		double result = exercise2.convertCelsiusToFahrenheit(celsius);
+		System.out.println("La conversión de los grados Centigrados a Farenheit es de: " + result + "°" + "\n");
+
+
 	}
 
 }
