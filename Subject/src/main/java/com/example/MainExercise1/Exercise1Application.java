d8cb40fad00add8a95e11f1cac72064a4e35cfe7
Juan Pablo <fonseca123papo@gmail.com>
2023-06-15 07:58:41 -0500

    Haciendocorrecciones de renombrar y espaciar en la carpeta MainExercise1

--- Subject/src/main/java/com/example/MainExercise1/Exercise1Application.java
+++ Subject/src/main/java/com/example/MainExercise1/Exercise1Application.java
@@ -1,4 +1,4 @@
-package com.example.Subject;
+package com.example.MainExercise1;
 
 import com.example.Exercise1.ConvertCelsiusToKelvin;
 import org.springframework.boot.SpringApplication;
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
 
 @SpringBootApplication
-public class SubjectApplication {
+public class Exercise1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SubjectApplication.class, args);
+		SpringApplication.run(Exercise1Application.class, args);
 
 		System.out.println("exercise 1: Convertir de grados centigrados a kelvin");
 		Scanner scannerCelius = new Scanner(System.in);
